e4f8f6e2539cb340420244c2a9f7a0de39ab3a32
MacTavish2992 <98577636+MacTavish2992@users.noreply.github.com>
2023-10-31 10:10:27 +0300

    PiggyBank
    PiggyBank

---
+++ ConsolePiggyBank/ConsolePiggyBank/Program.cs
@@ -0,0 +1,31 @@
+﻿
+DateTime datNow = new DateTime();
+DateTime datMiddle = new DateTime(2023, 01, 01);
+datNow = DateTime.Now;
+string info;
+int daysCount = Convert.ToInt32((datNow - datMiddle).TotalDays);
+for (int i = 0; i <= 365; ++i)
+{
+    if (i == daysCount)
+    {
+        if (daysCount > 99)
+        {
+            Console.WriteLine(daysCount+1 + " " + "день накопления");
+            info = Convert.ToString(daysCount+1);
+            info = info.Insert(1, ".");
+            Console.WriteLine(info + " " + "руб");
+        }
+        else
+        {
+            Console.WriteLine(daysCount+1 + " " + "день накопления");
+            Console.WriteLine(daysCount+1 + " " + "копеек");
+        }
+
+    }
+}
+bool Exit = false;
+while (!Exit)
+{
+    Exit = Console.ReadLine() == "exit";
+}
+
